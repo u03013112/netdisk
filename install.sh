1c012afa364a91a9ad3131c698f1b099e3220c30
宋志京 <>
2019-05-29 18:51:33 +0800

    =update

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
-
-SRC_FILE_DIR=http://u03013112.win/nd
-
 mkdir /home/nd -p
-touch /home/nd/filebrowser.db
+touch /home/nd/filebrowser.d
 
-curl -s ${SRC_FILE_DIR}/filebrowser.json -o /home/nd/filebrowser.json
-curl -s ${SRC_FILE_DIR}/docker-compose.yml -o /home/nd/docker-compose.yml
-curl -s ${SRC_FILE_DIR}/restart.sh -o /home/nd/restart.sh
+echo "please install docker & docker-compose first"
